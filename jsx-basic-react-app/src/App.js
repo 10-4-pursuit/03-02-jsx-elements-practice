3962432cb04d97bba14f5f9bbfb9513388ac7cf1
athomas227 <andrewthomas@pursuit.org>
2023-12-05 20:21:13 -0500

    finished exercise

--- jsx-basic-react-app/src/App.js
+++ jsx-basic-react-app/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 const name = "Andrew";
@@ -6,7 +7,7 @@ const user = { firstName: 'Andrew', lastName: 'Thomas' };
 const hello = <h1>Hello, World!</h1>;
 const greeting = <h1>Hello, {name}</h1>;
 const fullName = <h1>{formatName(user)}</h1>;
-const parts = <div><h2>Part 1</h2><h2>Part 2</h2></div>;
+const parts = <React.Fragment><h1>Part 1</h1><h1>Part 2</h1></React.Fragment>
 const image = <img src="https://livingsimplyy.files.wordpress.com/2016/09/6847284-samurai-champloo-wallpaper.jpg?w=1200" />;
 const click = <button onClick={buttonClicked}>Click Me</button>
 
