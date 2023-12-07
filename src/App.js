410501a4dd8756522384a5d7d4f469d794cad6ae
EdgarCollado <edgarcollado@pursuit.org>
2023-12-06 20:15:24 -0500

    worked-through-updated

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       <div>
         <img id='firstImg' class="page-img" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrSVHuIa8dxXoVax_Oe3nKFeg58_JzMWZBXQ&usqp=CAU' alt='Toji' style={{display: 'block', marginLeft: 'auto', marginRight: 'auto', width: 400}}></img>
       </div>
-      <div>
+      <div style={{textAlign: 'center', padding: 10}}>
         <button onClick={clickButton}>Click Here For More</button>
       </div>
     </div>
