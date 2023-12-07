7a02a2c76303d4d40fc1935ebf4f6ebcbe44408b
athomas227 <andrewthomas@pursuit.org>
2023-12-06 19:41:28 -0500

    fixed click constant

--- src/App.js
+++ src/App.js
@@ -7,16 +7,16 @@ const user = { firstName: 'Andrew', lastName: 'Thomas' };
 const hello = <h1>Hello, World!</h1>;
 const greeting = <h1>Hello, {name}</h1>;
 const fullName = <h1>{formatName(user)}</h1>;
-const parts = <React.Fragment><h1>Part 1</h1><h1>Part 2</h1></React.Fragment>
+const parts = ( 
+<React.Fragment>
+  <h1>Part 1</h1>
+  <h1>Part 2</h1>
+</React.Fragment>);
 const image = <img src="https://livingsimplyy.files.wordpress.com/2016/09/6847284-samurai-champloo-wallpaper.jpg?w=1200" />;
-const click = <button onClick={buttonClicked}>Click Me</button>
+const click = <button onClick={() => alert("Button Pressed")}>Click Me</button>;
 
 function formatName(user) {
   return user.firstName + ' ' + user.lastName;
-}
-
-function buttonClicked() {
-  console.log("Button clicked");
 }
 
 function App() {
