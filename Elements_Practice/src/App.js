a3499ac669e502d0c2865a66a9c7f974a7b88a50
Jpena101 <jannpena@pursuit.org>
2023-12-11 18:21:26 -0500

    Task Complete

---
+++ Elements_Practice/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+
+const HelloWorld = () => (
+  <div>
+    <h1>Hello, World!</h1>
+  </div>
+);
+
+const name = 'Jann Pena';
+const PersonalizedGreeting = () => 
+  <div>
+    <h1>Hello, {name}!</h1>
+  </div>
+
+const formatName = (user) => `${user.firstName} ${user.lastName}`;
+
+const user = {
+  firstName: 'Jann',
+  lastName: 'Pena',
+};
+
+const GreetingWithFunction = () => (
+  <div>
+    <h1>Hello, {formatName(user)}!</h1>
+  </div>
+)
+
+const ImageComponent = () => (
+  <div>
+     <img src="https://blog.depositphotos.com/wp-content/uploads/2017/07/Soothing-nature-backgrounds-2.jpg.webp" alt="A soothing nature background" />
+  </div>
+
+);
+;
+
+
+export default App;
